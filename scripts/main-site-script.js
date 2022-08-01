001c4fbbf144b79394fdfdbc586dd30e39779ffa
Andrzej Zuzański <95362301+andrzejzuzanski@users.noreply.github.com>
2022-08-01 09:14:45 +0200

    Changed eventListener element

--- scripts/main-site-script.js
+++ scripts/main-site-script.js
@@ -2,7 +2,7 @@ const mainContainer = document.querySelector("main");
 const welcomeContainer = document.querySelector(".welcome-container");
 mainContainer.classList.add("hidden");
 
-window.addEventListener("click", function () {
+document.body.addEventListener("click", function () {
   welcomeContainer.classList.add("scale-down-center");
   mainContainer.classList.remove("hidden");
   mainContainer.classList.add("slide-top");
