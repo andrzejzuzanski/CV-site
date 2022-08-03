ff4f71e2d3cf0daa6beeda49c7ddab9f01e29a0e
Andrzej Zuzański <andrzejzuzanski@gmail.com>
2022-08-03 18:27:20 +0200

    Added button listeners and hide function

--- scripts/main-site-script.js
+++ scripts/main-site-script.js
@@ -1,6 +1,22 @@
 const mainContainer = document.querySelector("main");
 const welcomeContainer = document.querySelector(".welcome-container");
+const profileContainer = document.querySelector(".profile-container");
+const aboutMeContainer = document.querySelector(".section-about-me");
+const contactContainer = document.querySelector(".section-contact");
+
+const aboutMeButton = document.querySelector(".about-me");
+const contactButton = document.querySelector(".contact");
+const mailButton = document.querySelector(".email");
+
+const profile = document.querySelector(".profile");
+const profileArrow = document.querySelector(".arrow");
+
 mainContainer.classList.add("hidden");
+
+aboutMeContainer.classList.add("hidden");
+aboutMeContainer.classList.add("position");
+contactContainer.classList.add("hidden");
+contactContainer.classList.add("position");
 
 document.body.addEventListener("click", function () {
   welcomeContainer.classList.add("scale-down-center");
@@ -8,12 +24,38 @@ document.body.addEventListener("click", function () {
   mainContainer.classList.add("slide-top");
 });
 
-const profile = document.querySelector(".profile");
-const profileContainer = document.querySelector(".profile-container");
-const profileArrow = document.querySelector(".arrow");
+//Containers
+const elements = [aboutMeContainer, contactContainer];
+
+function hideElements(elementContainer, allElements){
+    element = elementContainer;
+    element.classList.toggle("hidden");
+    element.classList.toggle("position");
+  
+  newElements = [...allElements];
+  console.log(newElements);
+
+    let elementsToProcess = newElements.filter((el) => el !== element)
+
+  console.log(elementsToProcess);
+  for(el of elementsToProcess){
+    el.classList.add("hidden");
+    el.classList.add("position");
+  }
+}
 
 profile.addEventListener("click", function () {
-    profileContainer.classList.toggle("profile-height");
-    profileArrow.style.transform += "rotate(180deg)";
-    profileArrow.style.transition = "transform 1s ";
-});  
+  profileContainer.classList.toggle("profile-height");
+  profileArrow.style.transform += "rotate(180deg)";
+  profileArrow.style.transition = "transform 1s ";
+});
+
+aboutMeButton.addEventListener("click", () => {
+    hideElements(aboutMeContainer, elements)})
+contactButton.addEventListener("click", () => {
+    hideElements(contactContainer, elements)})
+
+
+mailButton.addEventListener("click", (e) => {
+  navigator.clipboard.writeText("andrzejzuzanski@gmail.com");
+});
